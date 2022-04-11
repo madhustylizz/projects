66d86308fafb23669729b6c2ce013ff8e1ec7d43
Madhu <99789072+madhustylizz@users.noreply.github.com>
2022-04-11 10:10:11 +0530

    phase 4 practice projects

---
+++ phase4/data.js
@@ -0,0 +1,27 @@
+let currentQuestion = 0;
+let score = 0;
+ 
+let questions = [
+   {
+       question: "Is Coding Ninjas the best online learning platform?",
+       answers: [
+           {option: "For Sure!", answer: true},
+           {option: "No, not at all.", answer: false},
+       ]
+   },
+   {
+       question: "What is better if you wish to achieve success?",
+       answers: [
+           {option: "Hard Work", answer: false},
+           {option: "Smart Work", answer: true},
+       ]
+   },
+   {
+       question: "Complete the phrase: Time and ___ wait for none.",
+       answers: [
+           {option: "Batman", answer: false},
+           {option: "Tide", answer: true},
+       ]
+   }
+   
+]
